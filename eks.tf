c52400f40ed853c9c642028e375bda80fa522f4b
Guillermo Garcia <bitcoder@gmail.com>
2024-02-08 20:42:10 -0500

    Adding lock file and cluster/node definitions

---
+++ eks.tf
@@ -0,0 +1,45 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "experimental-eks"
+  cluster_version = "1.24"
+
+  vpc_id                          = module.vpc.vpc_id
+  subnet_ids                      = module.vpc.private_subnets
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  cluster_addons = {
+    coredns = {
+      resolve_conflict = "OVERWRITE"
+    }
+    vpc-cni = {
+      resolve_conflict = "OVERWRITE"
+    }
+    kube-proxy = {
+      resolve_conflict = "OVERWRITE"
+    }
+    csi = {
+      resolve_conflict = "OVERWRITE"
+    }
+  }
+
+  eks_managed_node_groups = {
+    node-group = {
+      desired_capacity = 1
+      max_capacity     = 2
+      min_capacity     = 1
+      instance_types   = ["t3.medium"]
+
+      tags = {
+        Environment = "experimental"
+      }
+    }
+  }
+
+  tags = {
+    Terraform   = "true"
+    Environment = "experimental"
+  }
+}
